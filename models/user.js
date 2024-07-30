cfab5d684f7a51e0f636ff8adfc68bd3d2125217
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-07-30 13:19:29 -0500

    Finished Skyrockit Exercises

--- models/user.js
+++ models/user.js
@@ -1,6 +1,31 @@
 const mongoose = require('mongoose');
+// models/user.js
 
-const userSchema = mongoose.Schema({
+// models/user.js
+
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+  },
+});
+
+
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -9,7 +34,9 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema], // embedding the applicationSchema here
 });
+
 
 const User = mongoose.model('User', userSchema);
 
